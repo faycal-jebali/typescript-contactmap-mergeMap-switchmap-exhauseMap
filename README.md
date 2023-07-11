ec9128d7f38516300c7ad608a71e61f7c6bd061e
faycal-jebali <39644707+faycal-jebali@users.noreply.github.com>
2023-07-11 16:42:44 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# typescript-contactmap-mergeMap-switchmap-exhauseMap
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-xahqbh)
---
+++ index.ts
@@ -0,0 +1,46 @@
+import { of } from 'rxjs';
+import {
+  concatMap,
+  delay,
+  mergeMap,
+  switchMap,
+  exhaustMap,
+} from 'rxjs/operators';
+
+//emit delay value
+const source = of(2000, 1000);
+// map value from source into inner observable, when complete emit result and move to next
+const example = source.pipe(
+  concatMap((val) => of(`Delayed by: ${val}ms`).pipe(delay(val)))
+);
+//output: With concatMap: Delayed by: 2000ms, With concatMap: Delayed by: 1000ms
+const subscribe = example.subscribe((val) =>
+  console.log(`With concatMap: ${val}`)
+);
+
+// showing the difference between concatMap and mergeMap
+const mergeMapExample = source
+  .pipe(
+    // just so we can log this after the first example has run
+    delay(5000),
+    mergeMap((val) => of(`Delayed by: ${val}ms`).pipe(delay(val)))
+  )
+  .subscribe((val) => console.log(`With mergeMap: ${val}`));
+
+// showing the difference between concatMap and mergeMap
+const switchMapExample = source
+  .pipe(
+    // just so we can log this after the first example has run
+    delay(5000),
+    switchMap((val) => of(`Delayed by: ${val}ms`).pipe(delay(val)))
+  )
+  .subscribe((val) => console.log(`With switchMap: ${val}`));
+
+// showing the difference between concatMap and mergeMap
+const exhaustMapExample = source
+  .pipe(
+    // just so we can log this after the first example has run
+    delay(5000),
+    exhaustMap((val) => of(`Delayed by: ${val}ms`).pipe(delay(val)))
+  )
+  .subscribe((val) => console.log(`With exhaustMap: ${val}`));
