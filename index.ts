8eeffeee020e629b65972ded886e65e00e26f9cb
faycal-jebali <39644707+faycal-jebali@users.noreply.github.com>
2023-07-11 16:44:48 +0200

    1 file modified

--- index.ts
+++ index.ts
@@ -5,12 +5,15 @@ import {
   mergeMap,
   switchMap,
   exhaustMap,
+  takeLast,
+  take,
 } from 'rxjs/operators';
 
 //emit delay value
-const source = of(2000, 1000);
+const source = of(500, 2000, 1000);
 // map value from source into inner observable, when complete emit result and move to next
 const example = source.pipe(
+  takeLast(2),
   concatMap((val) => of(`Delayed by: ${val}ms`).pipe(delay(val)))
 );
 //output: With concatMap: Delayed by: 2000ms, With concatMap: Delayed by: 1000ms
@@ -23,6 +26,7 @@ const mergeMapExample = source
   .pipe(
     // just so we can log this after the first example has run
     delay(5000),
+    takeLast(2),
     mergeMap((val) => of(`Delayed by: ${val}ms`).pipe(delay(val)))
   )
   .subscribe((val) => console.log(`With mergeMap: ${val}`));
@@ -32,6 +36,7 @@ const switchMapExample = source
   .pipe(
     // just so we can log this after the first example has run
     delay(5000),
+    takeLast(2),
     switchMap((val) => of(`Delayed by: ${val}ms`).pipe(delay(val)))
   )
   .subscribe((val) => console.log(`With switchMap: ${val}`));
@@ -41,6 +46,7 @@ const exhaustMapExample = source
   .pipe(
     // just so we can log this after the first example has run
     delay(5000),
+    take(1),
     exhaustMap((val) => of(`Delayed by: ${val}ms`).pipe(delay(val)))
   )
   .subscribe((val) => console.log(`With exhaustMap: ${val}`));
